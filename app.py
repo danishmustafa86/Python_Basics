2e6f7eff9dcdc93d9f7196c98c954d1d6656cc30
unknown <danishjajja86@gmail.com>
2024-10-14 20:54:00 +0500

    python basics pushes

---
+++ app.py
@@ -0,0 +1,22 @@
+# first_name = "John"
+# last_name = "Doe"
+# full_name = first_name + " " + last_name  # Concatenation: "John Doe"
+# print(full_name)
+
+# repeated_string = "Ha" * 3  # Repetition: "HaHaHa"
+# print(repeated_string)
+# print(repeated_string.upper())  # Output: ALICE
+# print(repeated_string.lower())  # Output: ALICE
+# print(name.lower())  # Output: alice
+
+# a = False
+# b = False
+# print(a and b)  # Output: False
+# print(a or b)   # Output: True
+# print(not a)    # Output: False
+
+print(int(5.99))  # Output: 5
+print(int("       9"))  # Output: 10
+print(float(10))         # Output: 10.0
+print(float("10.5"))     # Output: 10.5
+
